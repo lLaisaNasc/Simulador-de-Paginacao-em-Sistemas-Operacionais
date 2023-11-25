265311ccd1a9af78335b84fbbc00b4ba0703c1ab
Laisa Nascimento <llaisanasc@gmail.com>
2023-11-25 14:38:11 -0300

    feat: algoritmo Segunda Chance e arquivo txt

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 # -- Ler o arquivo --
 
-doc = open('./arquivos/fifo.txt', 'r', encoding='utf8')
+doc = open('./arquivos/sc.txt', 'r', encoding='utf8')
 algoritmo_substituicao = doc.readline().strip()
 
 # -- Dados do processo --
@@ -13,17 +13,18 @@ paginas_processo = [linha.replace('\u00a0', '').strip().split()
 # -- Idenfica o algoritmo de substituição --
 
 
-def identifica_algoritmo_substituicao(nome_substiuicao):
+def identifica_algoritmo_substituicao(nome_substiuicao, numero_de_quadros, paginas_processo):
     algoritmos = ['FIRST COME FIRST SERVED', 'FIFO',
-                  'SC', 'Segunda Chance', 'Relógio']
+                  'SC', 'Segunda Chance', '']
 
     if nome_substiuicao in algoritmos:
         posicao = algoritmos.index(nome_substiuicao)
 
         if posicao == 0 or posicao == 1:
-            utiliza_first_come_first_served()
+            utiliza_first_come_first_served(
+                numero_de_quadros, paginas_processo)
         elif posicao == 2 or posicao == 3:
-            utiliza_segunda_chance()
+            utiliza_segunda_chance(numero_de_quadros, paginas_processo)
         elif posicao == 4:
             ...
     else:
@@ -58,6 +59,9 @@ def utiliza_first_come_first_served(numero_de_quadros, paginas_processo):
             page_hits += 1
             print('page hit - página', pagina)
 
+    print('\nTotal Page Faults: ', page_faults)
+    print('Total Page Hits: ', page_hits)
+
     return fila, page_faults, page_hits
 
 # -- Algoritmo SC --
@@ -71,28 +75,42 @@ def utiliza_segunda_chance(numero_de_quadros, paginas_processo):
     print(paginas_processo)
 
     for operacao in paginas_processo:
+        # -- inicializa todas as páginas com bit 0 --
         pagina = [(eval(operacao[0])), 0]
 
+        # -- verifica se a página não está na fila e da um page fault --
         if pagina not in fila:
             page_faults += 1
             print('page fault - página', pagina)
 
+            # -- verifica se há espaço na fila e adiciona a nova página --
             if len(fila) < numero_de_quadros:
                 fila.append(pagina)
             else:
+                # -- lógica da segunda chance --
                 while (fila[0][1] != 0):
+                    fila[0][1] = 0
                     segunda_chance = fila.pop(0)
                     fila.append(segunda_chance)
 
-            fila.pop()
-            fila.append(pagina)
+            # -- remove a página mais antiga e adiciona a nova no final da fila --
+                fila.pop(0)
+                fila.append(pagina)
 
+         # -- se a página já está na fila da um page hit --
         else:
-            page_hits += 1
-            pagina[1] = 1
-            print('page hit - página', pagina)
+            for p in fila:
+                if p[0] == pagina[0]:
+                    p[1] = 1  # Atualiza o bit de referência da página na fila
+                    page_hits += 1
+                    print('page hit - página', p)
+                    break
+
+    print('\nTotal Page Faults: ', page_faults)
+    print('Total Page Hits: ', page_hits)
 
     return fila, page_faults, page_hits
 
 
-utiliza_first_come_first_served(numero_de_quadros, paginas_processo)
+identifica_algoritmo_substituicao(
+    algoritmo_substituicao, numero_de_quadros, paginas_processo)
